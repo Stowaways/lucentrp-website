7eeeac0aebc9b1994c455f876076da731f732ee2
Myles Deslippe <mdeslippe76@gmail.com>
2022-10-02 19:05:28 -0400

    Added a service worker

--- lucentrp-website/ClientApp/src/index.js
+++ lucentrp-website/ClientApp/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import App from './App';
 
 import './index.css';
@@ -13,3 +15,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// Register a service worker.
+serviceWorkerRegistration.register();
